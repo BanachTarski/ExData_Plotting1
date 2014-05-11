e43c57cce2d56e2e84062b311dcbfb463d0b68c6
Isaac Lawrence <isaaclawrence@gmail.com>
2014-05-11 11:22:47 -0700

    Upload assignment

---
+++ Plot1.R
@@ -0,0 +1,8 @@
+data <- read.table("household_power_consumption.txt",header=TRUE,sep=";")
+data$Date <- as.Date(data$Date,format="%d/%m/%Y")
+data <- subset(data,(data$Date == "2007-02-01" | data$Date == "2007-02-02"))
+data$Global_active_power <- as.character(data$Global_active_power)
+data$Global_active_power <- as.numeric(data$Global_active_power)
+png(file = "plot1.png")
+hist(data$Global_active_power,main = "Global Active Power",col = "Red",xlab = "Gloabl Active Power (kilowatts)")
+dev.off()
---
+++ Plot2.R
@@ -0,0 +1,11 @@
+data <- read.table("household_power_consumption.txt",header=TRUE,sep=";")
+data$Date <- as.Date(data$Date,format="%d/%m/%Y")
+data <- subset(data,(data$Date == "2007-02-01" | data$Date == "2007-02-02"))
+data$Global_active_power <- as.character(data$Global_active_power)
+data$Global_active_power <- as.numeric(data$Global_active_power)
+data$DateTime <- paste(data$Date,data$Time)
+data$DateTime <- strptime(data$DateTime,format = "%Y-%m-%d %H:%M:%S")
+png(file = "plot2.png")
+plot(data$DateTime,data$Global_active_power,type = "l",xlab = "",ylab = "Global Active Power (kilowatts)")
+dev.off()
+
